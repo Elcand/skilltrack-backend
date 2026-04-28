b6817da1f6f295e9c8e55947e14121b531953054
Mikhael Candra <tjandraraharja99@gmail.com>
2026-04-28 11:25:32 +0700

    feat: make relation user between skills

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,4 +29,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function skills()
+    {
+        return $this->hasMany(Skill::class);
+    }
 }
